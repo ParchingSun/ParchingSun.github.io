051c7ce09bd5f189db0cf12575a8178efd47c84c
孙盼成 <la_parching_sun@163.com>
2021-01-04 01:03:04 +0800

    add game library pc style
    big achievement!!!

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,30 +1,30 @@
 // the input of string is 'charAt' function
 // the display of cursor is achieved by creating one and then destroy it
+
 var charIndex = -1;
 var stringLength = 0;
 var inputText;
-
 function writeContent(init) {
   if (init) {
-    inputText = document.getElementById('contentToWrite').innerHTML;
+    inputText = document.getElementById(idselector).innerHTML;
   }
   if (charIndex == -1) {
     charIndex = 0;
     stringLength = inputText.length;
   }
-  var initString = document.getElementById('myContent').innerHTML;
+  var initString = document.getElementById('typefiled').innerHTML;
+
   initString = initString.replace(/<SPAN.*$/gi, "");
-  console.log(initString)
   var theChar = inputText.charAt(charIndex);
   var nextFourChars = inputText.substr(charIndex, 4);
   if (nextFourChars == '<br>') {
     theChar = '<br/>';
-    charIndex += 12;
+    charIndex += 10;
   }
   //define string to be inputed
   initString = initString + theChar + "<SPAN id='blink'>_</SPAN>";
   //input string
-  document.getElementById('myContent').innerHTML = initString;
+  document.getElementById('typefiled').innerHTML = initString;
   charIndex = charIndex / 1 + 1;
   if (charIndex % 2 == 1) {
     document.getElementById('blink').style.display = 'none';
