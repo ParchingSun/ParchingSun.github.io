6479505e7260e4b65d34a7b4508de2070585d431
sunpancheng <sunpancheng@dpstorm.com>
2021-01-09 14:05:54 +0800

    add click effect for box names

--- js/typewriter.js
+++ js/typewriter.js
@@ -4,6 +4,7 @@
 var charIndex = -1;
 var stringLength = 0;
 var inputText;
+var writeAction;
 function writeContent(init) {
   if (init) {
     inputText = document.getElementById(idselector).innerHTML;
@@ -36,4 +37,18 @@ function writeContent(init) {
   } else {
     document.getElementById('blink').style.display = 'none';
   }
+  return writeAction = 1;
+}
+var doesflur;
+function flur(){
+  document.getElementsByClassName('image')[0].style.filter = 'blur(5px) grayscale(100%)';
+  // document.getElementsByTagName('img')[2].style.display = 'none';
+  document.getElementsByClassName('container')[0].style.filter = 'blur(5px)';
+  // document.getElementsByClassName('container')[0].style.fliter= 'blur(1px)';
+  return doesflur = 1;
+}
+function deflur(){
+  document.getElementsByClassName('image')[0].style.filter = 'blur(0px) grayscale(0%)';
+  document.getElementsByClassName('container')[0].style.filter = 'blur(0px)';
+  return doesflur = 0;
 }
